66587f14db17c65094d2b19e9f5a4af968b8a28f
thissop <68932815+thissop@users.noreply.github.com>
2021-10-07 13:09:25 -0700

    :+1:

--- python_lessons/tess_lc_intro/notes.py
+++ python_lessons/tess_lc_intro/notes.py
@@ -1,18 +1,14 @@
 import numpy as np 
-import pandas as pd
 import matplotlib.pyplot as plt
 from astropy.timeseries import LombScargle 
 
 # generate random data 
 
-rand = np.random.default_rng(42)  
-
-dates = 100 * rand.random(100)
-fluxes = np.sin(2 * np.pi * dates) + 0.1 * rand.standard_normal(100)
+dates = 100 * np.random.random(100)
+fluxes = np.sin(2 * np.pi * dates) + 0.1 * np.random.standard_normal(100)
 
 # lomb scargle 
 
-frequencies, powers = LombScargle(dates, fluxes).autopower(minimum_frequency=0.25, maximum_frequency=5)
 periods = 1/frequencies
 
 best = np.argmax(powers)  
